35ed0fa09417c01f56ee7bdb6010d800e4725444
RishitJoshi-659 <rishitjoshi.23.cse@iite.indusuni.ac.in>
2024-09-22 18:55:18 +0530

    Add files via upload

---
+++ 5_3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Currency
+{
+private:
+    int rupees;
+    int paisa;
+
+public:
+    Currency() : rupees(0), paisa(0) {}
+
+    void get_currency()
+    {
+        cout << "Enter Rupees to add: ";
+        cin >> rupees;
+        cout << "Enter Paisa to add: ";
+        cin >> paisa;
+    }
+
+    int get_rupees() const { return rupees; } //its something called Accessor IDK GPT asked me to add this cuz it wasnt working before
+    int get_paisa() const { return paisa; }
+    
+    friend Currency add_currency(Currency obj1, Currency obj2); //using friend function
+};
+
+Currency add_currency(Currency obj1, Currency obj2)
+{
+    Currency obj_added;
+    obj_added.rupees = obj1.rupees + obj2.rupees;
+    obj_added.paisa = obj1.paisa + obj2.paisa;
+
+    if (obj_added.paisa >= 100) //to conver any paisa over 100 to rupees
+    {
+        obj_added.rupees += obj_added.paisa / 100; 
+        obj_added.paisa = obj_added.paisa % 100;   
+    }
+
+    return obj_added; //returning as object
+}
+
+int main()
+{
+    Currency obj1;
+    Currency obj2;
+    Currency obj_added;
+
+    obj1.get_currency();
+    obj2.get_currency();
+
+    obj_added = add_currency(obj1, obj2);
+
+    cout << "After adding:\n" 
+         << "Rupees: " << obj_added.get_rupees() 
+         << "\nPaisa: " << obj_added.get_paisa() << endl;
+
+    return 0;
+}
