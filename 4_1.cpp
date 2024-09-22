584f1f77679d0a4285f266f73ce3bbcdac724e97
RishitJoshi-659 <rishitjoshi.23.cse@iite.indusuni.ac.in>
2024-09-22 17:20:12 +0530

    Add files via upload

---
+++ 4_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    private:
+        double length;
+        double breadth;
+
+    public:
+        Rectangle(double l, double b) 
+        {
+            this->length = l; //using this pointer to assign values
+            this->breadth = b;
+        }
+
+        double area() 
+        {
+            return this->length * this->breadth; //using this pointer to calculate area
+        }
+};
+
+int main() 
+{
+    Rectangle obj_rect(5.0, 3.0);
+    cout << "Area: " << obj_rect.area() << endl;
+
+    return 0;
+}
