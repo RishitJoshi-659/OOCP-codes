dd3ca7a3bdfe33a643bade98cdf0d26781af3b91
RishitJoshi-659 <rishitjoshi.23.cse@iite.indusuni.ac.in>
2024-10-15 18:45:01 +0530

    Update 2_1.cpp

--- 2_1.cpp
+++ 2_1.cpp
@@ -15,15 +15,15 @@ int main()
     cout << "Enter the length of Square's side: ";
     cin >> s;
 
-    cout << "Enter the Rectange's length";
+    cout << "Enter the Rectange's length: ";
     cin >> l;
-    cout << "Enter the Rectange's Breadth";
+    cout << "Enter the Rectange's Breadth: ";
     cin >> b;
 
-    cout << "Enter radius of circle:";
+    cout << "Enter radius of circle: ";
     cin >> r;
 
-    cout << "Area of square is" << area(s) << endl;
+    cout << "\nArea of square is " << area(s) << endl;
     cout << "Area of rectangle is " << area(l, b) << endl;
     cout << "Area of circle is " << area(r) << endl;
 }
@@ -44,3 +44,18 @@ float area(float r)
 {
     return(3.14 * r * r);
 }
+
+/*
+
+Output Example:
+
+Enter the length of Square's side: 5
+Enter the Rectange's length: 10
+Enter the Rectange's Breadth: 5
+Enter radius of circle: 10
+
+Area of square is 25
+Area of rectangle is 50
+Area of circle is 314
+
+*/
