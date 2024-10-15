a37b5f60cd16e5556315d57837d2aca18d52c8e1
RishitJoshi-659 <rishitjoshi.23.cse@iite.indusuni.ac.in>
2024-10-15 21:14:42 +0530

    Add files via upload

---
+++ 9_2.cpp
@@ -0,0 +1,92 @@
+//GPT code modified by me
+
+#include <iostream>
+
+using namespace std;
+
+class Coord 
+{
+    private:
+        int x, y;
+
+    public:
+        // Constructor to initialize coordinates
+        Coord(int xVal = 0, int yVal = 0) : x(xVal), y(yVal) {}
+
+        // Display the current coordinates
+        void display() const 
+        {
+            cout << "Coordinates: (" << x << ", " << y << ")\n";
+        }
+
+        // Prefix ++ operator
+        Coord& operator++() 
+        {
+            ++x;
+            ++y;
+            return *this;
+        }
+
+        // Postfix ++ operator
+        Coord operator++(int) 
+        {
+            Coord temp = *this;
+            x++;
+            y++;
+            return temp;
+        }
+
+        // Prefix -- operator
+        Coord& operator--() 
+        {
+            --x;
+            --y;
+            return *this;
+        }
+
+        // Postfix -- operator
+        Coord operator--(int) 
+        {
+            Coord temp = *this;
+            x--;
+            y--;
+            return temp;
+        }
+};
+
+int main() 
+{
+    int x, y;
+
+    // Input initial coordinates
+    cout << "Enter initial x and y coordinates: ";
+    cin >> x >> y;
+
+    Coord point(x, y);
+
+    // Display original coordinates
+    cout << "Original ";
+    point.display();
+
+    // Demonstrate prefix increment
+    cout << "\nApplying prefix ++\n";
+    ++point;
+    point.display();
+
+    // Demonstrate postfix increment
+    cout << "\nApplying postfix ++\n";
+    point++;
+    point.display();
+
+    // Demonstrate prefix decrement
+    cout << "\nApplying prefix --\n";
+    --point;
+    point.display();
+
+    // Demonstrate postfix decrement
+    cout << "\nApplying postfix --\n";
+    point--;
+    point.display();
+
+    return 0;
+}
