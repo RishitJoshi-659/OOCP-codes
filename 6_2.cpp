fcb71ec14ffc16c7f215fc8f6d1dce2014e0249b
RishitJoshi-659 <rishitjoshi.23.cse@iite.indusuni.ac.in>
2024-10-15 19:07:37 +0530

    Update and rename 6_2.CPP to 6_2.cpp

--- 6_2.cpp
+++ 6_2.cpp
@@ -81,3 +81,22 @@ int main()
     f1.compute_pay();
     return 0;
 }
+
+/*
+Output Example:
+
+Enter Id: 2345
+Enter Name: Zhongli
+Enter Salary per hour: 1000
+Enter the number of days worked: 28
+Enter the number of hours worked per day: 6
+Male Employee earned 168000 Rupees
+
+Enter Id: 4522 
+Enter Name: Fischl
+Enter Salary per hour: 1200
+Hours Worked: 43
+Salary will be counted till 40 hrs only
+
+Female Employee earned 48000 Rupees
+*/
